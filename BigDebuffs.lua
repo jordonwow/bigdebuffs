059d6a2a0d7fb11278e9052daba1b7614aa9d711
domzae <tha_domzae@hotmail.com>
2018-10-04 09:26:35 +0200

    Minor spell update

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -131,7 +131,7 @@ BigDebuffs.Spells = {
 	[48707] = { type = "immunities_spells" }, -- Anti-Magic Shell
 	[48265] = { type = "buffs_defensive" }, -- Death's Advance
 	[48792] = { type = "buffs_defensive" }, -- Icebound Fortitude
-	[49028] = { type = "buffs_offensive" }, -- Dancing Rune Weapon
+	[81256] = { type = "buffs_defensive" }, -- Dancing Rune Weapon
 	[51271] = { type = "buffs_offensive" }, -- Pillar of Frost
 	[55233] = { type = "buffs_defensive" }, -- Vampiric Blood
 	[77606] = { type = "buffs_other" }, -- Dark Simulacrum
@@ -201,6 +201,7 @@ BigDebuffs.Spells = {
 	[102560] = { type = "buffs_offensive" }, -- Incarnation: Chosen of Elune
 	[106951] = { type = "buffs_offensive" }, -- Berserk
 	[155835] = { type = "buffs_defensive" }, -- Bristling Fur
+	[192081] = { type = "buffs_defensive" }, -- Ironfur
 	[163505] = { type = "cc" }, -- Rake
 	[194223] = { type = "buffs_offensive" }, -- Celestial Alignment
 	[200851] = { type = "buffs_defensive" }, -- Rage of the Sleeper
@@ -220,13 +221,14 @@ BigDebuffs.Spells = {
 
 	[136] = { type = "buffs_defensive" }, -- Mend Pet
 	[3355] = { type = "cc" }, -- Freezing Trap
+		[203340] = { type ="cc" }, -- Diamond Ice (Survival Honor Talent)
 	[5384] = { type = "buffs_defensive" }, -- Feign Death
 	[19386] = { type = "cc" }, -- Wyvern Sting
 	[19574] = { type = "buffs_offensive" }, -- Bestial Wrath
 	[19577] = { type = "cc" }, -- Intimidation
 		[24394] = { type = "cc", parent = 19577 }, -- Intimidation
 	[53480] = { type = "buffs_defensive" }, -- Roar of Sacrifice (Hunter Pet Skill)
-	[117526] = { type = "roots" }, -- Binding Shot 
+	[117526] = { type = "roots" }, -- Binding Shot
 	[131894] = { type = "buffs_offensive" }, -- A Murder of Crows (Beast Mastery, Marksmanship)
 		[206505] = { type = "buffs_offensive", parent = 131894 }, -- A Murder of Crows (Survival)
 	[186265] = { type = "buffs_defensive" }, -- Aspect of the Turtle
@@ -260,6 +262,8 @@ BigDebuffs.Spells = {
 		[161354] = { type = "cc", parent = 118 }, -- Polymorph Monkey
 		[161355] = { type = "cc", parent = 118 }, -- Polymorph Penguin
 		[161372] = { type = "cc", parent = 118 }, -- Polymorph Peacock
+		[277787] = { type = "cc", parent = 118 }, -- Polymorph Direhorn
+		[277792] = { type = "cc", parent = 118 }, -- Polymorph Bumblebee
 	[122] = { type = "roots" }, -- Frost Nova
 		[33395] = { type = "roots", parent = 122 }, -- Freeze
 	[11426] = { type = "buffs_defensive" }, -- Ice Barrier
@@ -491,7 +495,8 @@ BigDebuffs.Spells = {
 		[232633] = { type = "cc", parent = 129597 }, -- Arcane Torrent
 	[192001] = { type = "buffs_other" }, -- Drink
 		[167152] = { type = "buffs_other", parent = 192001 }, -- Refreshment
-	[257040] = { type = "buffs_other" }, -- Spatial Rift
+	[256948] = { type = "buffs_other" }, -- Spatial Rift
+	[255654] = { type = "cc" }, --Bull Rush
 
 	-- Legacy (may be deprecated)
 
@@ -504,13 +509,13 @@ BigDebuffs.Spells = {
 local specDispel = {
 	[62] = { -- Arcane Mage
 		Curse = true,
-	},		
+	},
 	[63] = { -- Fire Mage
 		Curse = true,
-	},		
+	},
 	[64] = { -- Frost Mage
 		Curse = true,
-	},		
+	},
 	[65] = { -- Holy Paladin
 		Magic = true,
 		Poison = true,
@@ -900,7 +905,8 @@ end
 local function UnitDebuffTest(unit, index)
 	local debuff = TestDebuffs[index]
 	if not debuff then return end
-	return "Test", nil, debuff[2], 0, debuff[4], 0, 0, nil, nil, nil, debuff[1]
+	-- name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, _, spellId
+	return "Test", debuff[2], 0, debuff[4], 0, 0, nil, nil, nil, debuff[1]
 end
 
 function BigDebuffs:OnEnable()
@@ -913,8 +919,9 @@ function BigDebuffs:OnEnable()
 	self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	self:PLAYER_TALENT_UPDATE()
 
+	-- (finish animations deprecated in latest OmniCC)
 	-- Prevent OmniCC finish animations
-	if OmniCC then
+	if OmniCC and OmniCC.TriggerEffect then
 		self:RawHook(OmniCC, "TriggerEffect", function(object, cooldown)
 			local name = cooldown:GetName()
 			if name and name:find("BigDebuffs") then return end
@@ -938,12 +945,20 @@ function BigDebuffs:PLAYER_ENTERING_WORLD()
 	end
 end
 
-function BigDebuffs:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, ...)
+local function UnitBuffByName(unit, name)
+	for i = 1, 40 do
+		local n = UnitBuff(unit, i)
+		if n == name then return end
+	end
+end
+
+function BigDebuffs:COMBAT_LOG_EVENT_UNFILTERED()
+
+	local _, event, _,_,_,_,_, destGUID, _,_,_, spellId = CombatLogGetCurrentEventInfo()
 
 	-- SPELL_INTERRUPT doesn't fire for some channeled spells
 	if event ~= "SPELL_INTERRUPT" and event ~= "SPELL_CAST_SUCCESS" then return end
 
-	local _,_,_,_,_, destGUID, _,_,_, spellId = ...
 	local spell = self.Spells[spellId]
 
 	if not spell or spell.type ~= "interrupts" then return end
@@ -955,12 +970,12 @@ function BigDebuffs:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, ...)
 			local duration = spell.duration
 			local _, class = UnitClass(unit)
 
-			if class == "PRIEST" or class == "SHAMAN" or class == "WARLOCK" then
-				duration = duration * 0.7
-			end
+			-- if class == "PRIEST" or class == "SHAMAN" or class == "WARLOCK" then
+			-- 	duration = duration * 0.7
+			-- end
 
-			if UnitBuff(unit, "Burning Determination") or UnitBuff(unit, "Calming Waters") or UnitBuff(unit, "Casting Circle") then
-				duration = duration * 0.3
+			if UnitBuffByName(unit, "Calming Waters") then
+				duration = duration * 0.5
 			end
 
 			self.units[destGUID] = self.units[destGUID] or {}
@@ -1162,8 +1177,7 @@ local function CompactUnitFrame_UtilSetDebuff(debuffFrame, unit, index, filter, 
 	-- make sure you are using the correct index here!
 	--isBossAura says make this look large.
 	--isBossBuff looks in HELPFULL auras otherwise it looks in HARMFULL ones
-	local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, shouldConsolidate, spellId;
-
+	local name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, _, spellId;
 	if index == -1 then
 		-- it's an interrupt
 		local spell = BigDebuffs.units[UnitGUID(unit)]
@@ -1174,9 +1188,9 @@ local function CompactUnitFrame_UtilSetDebuff(debuffFrame, unit, index, filter, 
 		expirationTime = spell.expires
 	else
 		if (isBossBuff) then
-			name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, shouldConsolidate, spellId = UnitBuff(unit, index, filter);
+			name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, _, spellId = UnitBuff(unit, index, filter);
 		else
-			name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, shouldConsolidate, spellId = UnitDebuff(unit, index, filter);
+			name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, _, spellId = UnitDebuff(unit, index, filter);
 		end
 	end
 
@@ -1184,7 +1198,7 @@ local function CompactUnitFrame_UtilSetDebuff(debuffFrame, unit, index, filter, 
 	debuffFrame.icon:SetTexture(icon);
 	if ( count > 1 ) then
 		local countText = count;
-		if ( count >= 10 ) then
+		if ( count >= 100 ) then
 			countText = BUFF_STACKS_OVERFLOW;
 		end
 		debuffFrame.count:Show();
@@ -1193,12 +1207,12 @@ local function CompactUnitFrame_UtilSetDebuff(debuffFrame, unit, index, filter, 
 		debuffFrame.count:Hide();
 	end
 	debuffFrame:SetID(index);
-	if ( expirationTime and expirationTime ~= 0 ) then
+	local enabled = expirationTime and expirationTime ~= 0;
+	if enabled then
 		local startTime = expirationTime - duration;
-		debuffFrame.cooldown:SetCooldown(startTime, duration);
-		debuffFrame.cooldown:Show();
+		CooldownFrame_Set(debuffFrame.cooldown, startTime, duration, true);
 	else
-		debuffFrame.cooldown:Hide();
+		CooldownFrame_Clear(debuffFrame.cooldown);
 	end
 
 	local color = DebuffTypeColor[debuffType] or DebuffTypeColor["none"];
@@ -1206,7 +1220,8 @@ local function CompactUnitFrame_UtilSetDebuff(debuffFrame, unit, index, filter, 
 
 	debuffFrame.isBossBuff = isBossBuff;
 	if ( isBossAura ) then
-		debuffFrame:SetSize(debuffFrame.baseSize + BOSS_DEBUFF_SIZE_INCREASE, debuffFrame.baseSize + BOSS_DEBUFF_SIZE_INCREASE);
+		local size = min(debuffFrame.baseSize + BOSS_DEBUFF_SIZE_INCREASE, debuffFrame.maxHeight);
+		debuffFrame:SetSize(size, size);
 	else
 		debuffFrame:SetSize(debuffFrame.baseSize, debuffFrame.baseSize);
 	end
