2274db2628ecdc91ac52616d5d0bdfe7ca334587
jordon <29447509+jordonwow@users.noreply.github.com>
2019-03-08 22:38:41 -0800

    Fix incorrectly detecting if a player is immune to interrupt

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -996,7 +996,7 @@ function BigDebuffs:COMBAT_LOG_EVENT_UNFILTERED()
 	-- Find unit
 	for i = 1, #unitsWithRaid do
 		local unit = unitsWithRaid[i]
-		if destGUID == UnitGUID(unit) and (event ~= "SPELL_CAST_SUCCESS" or select(8, UnitChannelInfo(unit)) == false) then
+		if destGUID == UnitGUID(unit) and (event ~= "SPELL_CAST_SUCCESS" or select(7, UnitChannelInfo(unit)) == false) then
 			local duration = spell.duration
 			local _, class = UnitClass(unit)
 
