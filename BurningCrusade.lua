d3da38b595cfdebe6d75ce67853fd5eb9987a63e
jordon <29447509+jordonwow@users.noreply.github.com>
2021-06-24 08:21:57 -0700

    add more bcc spells

--- BurningCrusade.lua
+++ BurningCrusade.lua
@@ -66,6 +66,7 @@ addon.Spells = {
 
     -- Other
 
+    [33961] = { type = IMMUNITY_SPELL }, -- Spell Reflection (Sethekk Initiate)
     [23451] = { type = BUFF_OFFENSIVE }, -- Battleground Speed buff
     [23493] = { type = BUFF_DEFENSIVE }, -- Battleground Heal buff
     [23505] = { type = BUFF_OFFENSIVE }, -- Battleground Damage buff
@@ -98,6 +99,8 @@ addon.Spells = {
     [9774] = { type = BUFF_OFFENSIVE }, -- Spider Belt & Ornate Mithril Boots
     [18798] = { type = CROWD_CONTROL }, -- Freezing Band
     [22734] = { type = BUFF_OTHER }, -- Drink
+        [46755] = { parent = 22734 }, -- Drink
+        [27089] = { parent = 22734 }, -- Drink
     [23605] = { type = BUFF_OTHER }, -- Nightfall, Spell Vulnerability
     [13494] = { type = BUFF_OFFENSIVE }, -- Manual Crowd Pummeler Haste buff
 
@@ -212,6 +215,7 @@ addon.Spells = {
         [30413] = { parent = 30283 },
         [30414] = { parent = 30283 },
     [43523] = { type = CROWD_CONTROL }, -- Unstable Affliction
+        [31117] = { parent = 43523 },
     [30300] = { type = IMMUNITY_SPELL }, -- Nether Protection
     [18708] = { type = BUFF_DEFENSIVE }, -- Fel Domination
     [32752] = { type = CROWD_CONTROL }, -- Summoning Disorientation
@@ -224,7 +228,7 @@ addon.Spells = {
 
     -- Shaman
 
-    [8178] = { type = IMMUNITY }, -- Grounding Totem Effect
+    [8178] = { type = IMMUNITY_SPELL }, -- Grounding Totem Effect
     [16188] = { type = BUFF_DEFENSIVE }, -- Nature's Swiftness
     [12548] = { type = ROOT }, -- Frost Shock
     [39796] = { type = CROWD_CONTROL }, -- Stoneclaw Totem
@@ -259,6 +263,7 @@ addon.Spells = {
 
     -- Hunter
 
+    [27046] = { type = BUFF_DEFENSIVE }, -- Mend Pet
     [13159] = { type = BUFF_OFFENSIVE }, -- Aspect of the Pack
         [5118] = { parent = 13159 }, -- Aspect of the Cheetah
     [1513] = { type = CROWD_CONTROL }, -- Scare Beast
