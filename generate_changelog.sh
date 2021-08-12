b6fcbc0976d33f8f9c763e46693379e862987069
jordon <29447509+jordonwow@users.noreply.github.com>
2021-08-12 01:38:15 -0700

    shorter changelog

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -23,7 +23,7 @@ fi
 date=$( git log -1 --date=short --format="%ad" )
 url=$( git remote get-url origin | sed -e 's/^git@\(.*\):/https:\/\/\1\//' -e 's/\.git$//' )
 
-echo -ne "# [${version}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n" > "CHANGELOG.md"
+# echo -ne "# [${version}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n" > "CHANGELOG.md"
 
 if [ "$version" = "$tag" ]; then # on a tag
   git cat-file -p "$tag" | sed -e '1,5d' -e '/^-----BEGIN PGP/,/^-----END PGP/d' >> "CHANGELOG.md"
