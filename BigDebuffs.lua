2508320c8f9f0aa2c9396679e18ee56b6964abb8
anzz1 <anzz1@live.com>
2022-09-03 03:06:44 +0300

    wotlk talent fixes
    - paladin improved concentration aura
    - shaman focused mind

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -1073,6 +1073,20 @@ function BigDebuffs:COMBAT_LOG_EVENT_UNFILTERED()
                         duration = duration * (1.0 - 0.1 * focusedMindRank)
                     end
                 end
+            elseif WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC then
+                local _, playerClass = UnitClass("player")
+                if playerClass == "PALADIN" then
+                    local improvedConcAuraRank = select(5, GetTalentInfo(1, 8))
+                    if improvedConcAuraRank > 0 and UnitBuffByName(unit, "Concentration Aura") then
+                        duration = duration * (1.0 - 0.1 * improvedConcAuraRank)
+                    end
+                end
+                if playerClass == "SHAMAN" then
+                    local focusedMindRank = select(5, GetTalentInfo(3, 16))
+                    if focusedMindRank > 0 then
+                        duration = duration * (1.0 - 0.1 * focusedMindRank)
+                    end
+                end
             end
 
             self.units[destGUID] = self.units[destGUID] or {}
