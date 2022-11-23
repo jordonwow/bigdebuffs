226c26f0a7f8d27ea4ef0ef1e1dfd1c6c42ffea2
birdfighter <johan.norin.90@gmail.com>
2022-11-23 15:34:16 +0100

    Fixed ElvUI anchor when no player in party frame

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -345,7 +345,6 @@ local GetAnchor = {
                     end
                 end
             end
-            return
         end
 
         if unit and (unit:match("arena") or unit:match("arena")) then
